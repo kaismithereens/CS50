771032f67c8f050226abe4ec6ddca7afd74f36cf
Kai <kai.nevermind@gmail.com>
2024-01-31 11:41:32 +0100

    Problem set 1

---
+++ bank.py
@@ -0,0 +1,17 @@
+"""
+In a file called bank.py, implement a program that prompts the user for a greeting.
+If the greeting starts with “hello”, output $0. If the greeting starts with an “h” (but not “hello”), output $20.
+Otherwise, output $100.
+Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
+"""
+
+answer = input("What is your greeting? ").lower().strip()
+
+if answer.startswith("hello"):
+    print("$0 ")
+elif answer.startswith("h"):
+    print("$20 ")
+else:
+    print("$100 ")
+
+
